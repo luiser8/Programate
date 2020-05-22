2d3009b2518e040697ff0a2bd8a053033c0e41a9
luiser8 <Administrador@LTHGBLA-SERV01.avior.com.ve>
2020-05-22 19:10:35 -0400

    inicial

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -5,10 +5,10 @@ import Presentacion from './Presentacion';
 const Home = () => {
 
     return (
-        <div>
-            <div className="p-md-3 text-black rounded bg-white">
+        <div style={{marginTop:"160px"}}>
+            <div className="p-md-3 text-black rounded bg-white" style={{marginBottom:"160px"}}>
                 <div className="col-md-6 px-0 d-inline-block text-center">
-                    <h1 className="text-center display-4 font-italic">Programar es el futuro_</h1>
+                    <h1 className="text-center display-4 font-italic">Programar es el futuro</h1>
                     <p className="lead my-3">Escuela Web es un centro de formación con Clases presenciales y en línea Contamos con más de xx alumnos egresados</p>
                     <a href="/cursos" className="lead my-3">Explorar cursos</a>
                 </div>
